e64c7605d52e2a88c0c2fa0bd5138f8603bcf450
GTsirekidze <g_tsirekidze@cu.edu.ge>
2022-09-21 04:34:21 +0400

    "commit"

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        if(month == 2){
+            System.out.println(year==1900 || year%4!= 0? 28: 29);
+        } else if(month<=7 && month%2==1 || (month>7 && month%2==0)){
+            System.out.println(31);
+        } else if(month>=1 && month<=12){
+            System.out.println(30);
+        } else{
+            System.out.println("invalid date");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,7 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+        double mx = Math.max(Math.max(secondSide,firstSide),thirdSide);
+        System.out.println(mx<firstSide+secondSide+thirdSide-mx?"this is a valid triangle": "it's not a triangle");
     }
 }
